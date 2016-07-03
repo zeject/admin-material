b3c996d346402a41fe3f353c1a7ca7d523c9b02a
zeject <424049902@qq.com>
2016-07-03 12:58:57 +0800

    date update

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,25 @@
 /**
  * You must include the dependency on 'ngMaterial'
  */
-var app = angular.module('app', [ 'ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages', 'md.data.table', 'mdPickers', 'ui.router' ]);
+var app = angular.module('app', [ 'ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages', 'md.data.table', 'ui.router', 'smDateTimeRangePicker']);
 
-app.config(function() {
+app.config(function ($mdThemingProvider,pickerProvider) { 
 
+     pickerProvider.setOkLabel('确定'); 
+     pickerProvider.setCancelLabel('取消');
+     pickerProvider.setDayHeader('single'); //Options 'single','shortName', 'fullName' 
+     pickerProvider.setDaysNames([ 
+      {'single':'日','shortName':'日','fullName':'Sunday'}, 
+      {'single':'一','shortName':'一','fullName':'MonDay'}, 
+      {'single':'二','shortName':'二','fullName':'TuesDay'}, 
+      {'single':'三','shortName':'三','fullName':'Wednesday'}, 
+      {'single':'四','shortName':'四','fullName':'Thursday'}, 
+      {'single':'五','shortName':'五','fullName':'Friday'}, 
+      {'single':'六','shortName':'六','fullName':'Saturday'} 
+     ]);
+     pickerProvider.setDivider('--'); 
+     pickerProvider.setMonthNames(["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"]); 
+     pickerProvider.setRangeCustomStartEnd(['起始日期', '结束日期']); 
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -36,6 +51,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 app.controller('ctrl.center', function($scope, $timeout, $mdDialog, $mdEditDialog) {
 	
+	var vm = $scope.vm = {};
+
 	$scope.myDate = new Date();
 
 	var imagePath = 'app/img/demo.jpg';
